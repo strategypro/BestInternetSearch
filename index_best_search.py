5591d27723acf9010471bdedebcf844ffdd89f8c
Stan S <vfpro777@yahoo.com>
2020-01-23 19:27:01 -0500

    added link to source code on front page

--- index_best_search.py
+++ index_best_search.py
@@ -21,20 +21,20 @@ def enc_print(string='', encoding='utf8'):
 
 html = ''
 
-domain = os.environ['HTTP_HOST'].lower()
+strDomain = os.environ['HTTP_HOST'].lower()
 
 title = ''
-heading1 = ''
+domain_name = ''
 
-if domain == 'www.bestinternetsearch.com' or domain == 'bestinternetsearch.com':
+if strDomain == 'www.bestinternetsearch.com' or strDomain == 'bestinternetsearch.com':
     title = 'Best Internet Search'
-    heading1 = 'BestInternetSearch.com'
-elif domain == 'www.bestwwwsearch.com' or domain == 'bestwwwsearch.com':
+    domain_name = 'BestInternetSearch.com'
+elif strDomain == 'www.bestwwwsearch.com' or strDomain == 'bestwwwsearch.com':
     title = 'Best www Search'
-    heading1 = 'BestwwwSearch.com'
-elif domain == 'www.bestnetsearch.com' or domain == 'bestnetsearch.com':
+    domain_name = 'BestwwwSearch.com'
+elif strDomain == 'www.bestnetsearch.com' or strDomain == 'bestnetsearch.com':
     title = 'Best net Search'
-    heading1 = 'BestnetSearch.com'
+    domain_name = 'BestnetSearch.com'
 
 arguments = cgi.FieldStorage()
 q = '' if not arguments.getvalue( "q" ) else escape( arguments.getvalue( "q" ) )
@@ -76,8 +76,8 @@ if not q == '':
     </script>
     
     
-    <link rel="icon" type="image/png" sizes="32x32" href="http://{domain}/bestsearch/page_img/favicon.png">
-    <script src="http://{domain}/bestsearch/js/jquery-3.4.1.min.js"></script>
+    <link rel="icon" type="image/png" sizes="32x32" href="http://{strDomain}/bestsearch/page_img/favicon.png">
+    <script src="http://{strDomain}/bestsearch/js/jquery-3.4.1.min.js"></script>
     </head>
     <body>
     """
@@ -91,7 +91,7 @@ if not q == '':
     cur.execute(f"""SELECT * from search WHERE LOWER(site_name) LIKE '%{q}%' ;""")
     res = cur.fetchall()   
     if res:
-        html += f"""Under construction, <a href="http://{domain}">{heading1}</a> created in 2020<br><br>"""
+        html += f"""Under construction, <a href="http://{strDomain}">{domain_name}</a> created in 2020<br><br>"""
 
         html += f"""
         <div class="items">"""
@@ -118,7 +118,6 @@ else:
     <head>
     <meta charset="UTF-8">
     <title>{title}</title>
-
 
     <script>
         if (/Mobi/i.test(navigator.userAgent) || /Android/i.test(navigator.userAgent))
@@ -149,6 +148,10 @@ else:
                         font-size: 200%;
                     }}
                     
+                    .git_source_code {{
+                        text-align: right;
+                    }}                       
+                    
                 </style>
             `);
         }}
@@ -172,21 +175,20 @@ else:
                         border:1px solid grey;    
                     }}
                     
+                    .git_source_code {{
+                        text-align: right;
+                    }}                    
+                    
                 </style>
             `);
         }}
     </script>
-
-
-
     
-
+    <link rel="icon" type="image/png" sizes="32x32" href="http://{strDomain}/bestsearch/page_img/favicon.png">
+    <script src="http://{strDomain}/bestsearch/js/jquery-3.4.1.min.js"></script>
     
-    <link rel="icon" type="image/png" sizes="32x32" href="http://{domain}/bestsearch/page_img/favicon.png">
-    <script src="http://{domain}/bestsearch/js/jquery-3.4.1.min.js"></script>
-    
-    <link rel="stylesheet" href="http://{domain}/bestsearch/js/awesomplete/awesomplete.css" />
-    <script src="http://{domain}/bestsearch/js/awesomplete/awesomplete.min.js"></script>
+    <link rel="stylesheet" href="http://{strDomain}/bestsearch/js/awesomplete/awesomplete.css" />
+    <script src="http://{strDomain}/bestsearch/js/awesomplete/awesomplete.min.js"></script>
     
     <script>    
         $(document).ready(function() {{
@@ -196,71 +198,70 @@ else:
             }});
 
         }});
-    </script>
+
+        
+        $(document).ready(function() {{
+        
+            var list = [];                                                                                 
+            
+            list.push({{label:"<h6 style='display:inline;'>suggestions - trending</h6>", value:"" }});
+            list.push({{label:"EarthDay.Love", value:"http://EarthDay.Love" }}); 
+            //list.push({{label:"TotalMart.US", value:"http://TotalMart.US" }}); 
+
+            var autocomplete = new Awesomplete(document.querySelector("#search_input"), {{
+                        list:list,
+                        minChars: 2,
+                        maxItems: 20,
+                        sort: false,
+                        replace: function(suggestion) {{
+                            //suggestion.value
+                            this.input.value = suggestion.label;
+                        }}
+                }}
+                );
     
-        <script>
-            
-            $(document).ready(function() {{
-            
-                var list = [];                                                                                 
+            $( "#search_input" ).val('  ');
+    
+            $( "#search_input" ).focus(function() {{
                 
-                list.push({{label:"<h6 style='display:inline;'>suggestions - trending</h6>", value:"" }});
-                list.push({{label:"EarthDay.Love", value:"http://EarthDay.Love" }}); 
-                //list.push({{label:"TotalMart.US", value:"http://TotalMart.US" }}); 
-
-                var autocomplete = new Awesomplete(document.querySelector("#search_input"), {{
-                            list:list,
-                            minChars: 2,
-                            maxItems: 20,
-                            sort: false,
-                            replace: function(suggestion) {{
-                                //suggestion.value
-                                this.input.value = suggestion.label;
+                autocomplete.list = list;                   
+                autocomplete.open();                   
+                $( this ).val('');
+            }});         
+    
+            $("#search_input").keyup(function(e) {{
+                
+                if ( $(this).val().length < 2) {{
+                    return;
+                }}
+                
+                var code = (e.keyCode || e.which);  // get keycode of current keypress event
+                if(code == 37 || code == 38 || code == 39 || code == 40) {{  // it's an arrow key
+                    return;
+                }}
+            
+                $.get( "http://{strDomain}/bestsearch/py/autocomplete_best_search.py?q=" + $("#search_input").val(), function( data ) {{
+                    
+                            var data = JSON.parse( data.trim() );				 
+                            var list = [];                                                                                 
+                            for (var i=0; i<data.length; i++) {{
+                                list.push({{label:data[i].label, value:data[i].value }})
                             }}
-                    }}
-                    );
-        
-                $( "#search_input" ).focus(function() {{
-
-                    autocomplete.list = list;                   
-                    autocomplete.open();                   
-
-                }});         
-        
-                $("#search_input").keyup(function(e) {{
-                    
-                    //if ( $(this).val().length < 2) {{
-                    //    return;
-                    //}}
-                    
-                    var code = (e.keyCode || e.which);  // get keycode of current keypress event
-                    if(code == 37 || code == 38 || code == 39 || code == 40) {{  // it's an arrow key
-                        return;
-                    }}
+                            
+                            autocomplete.list = list;
+                            
+                }});
                 
-                    $.get( "http://{domain}/bestsearch/py/autocomplete_best_search.py?q=" + $("#search_input").val(), function( data ) {{
-                        
-                                var data = JSON.parse( data.trim() );				 
-                                var list = [];                                                                                 
-                                for (var i=0; i<data.length; i++) {{
-                                    list.push({{label:data[i].label, value:data[i].value }})
-                                }}
-                                
-                                autocomplete.list = list;
-                                
-                    }});
-                    
-                }});     
-        
-        
-        
-            }});
-        </script>    
+            }});     
+    
+        }});
+    </script>    
     
     </head>
     <body>
+    <div class="git_source_code"><a  href="https://github.com/strategypro/BestInternetSearch">source code</a></div>
 <pre>
-<h1><a href="http://{domain}">{heading1}</a></h1>
+<h1><a href="http://{strDomain}">{domain_name}</a></h1>
 </pre>
 
     <h1 class="center_title">{title}</h1>
@@ -268,7 +269,7 @@ else:
     <br>
     
     <form id="search_form" action="/" method="get">
-    <input id="search_input" type="text" name="q" style="height:35px"><img id="search_img" src="http://{domain}/bestsearch/page_img/search.png" >
+    <input id="search_input" type="text" name="q" style="height:35px"><img id="search_img" src="http://{strDomain}/bestsearch/page_img/search.png" >
     <br>
     <input id="search_submit" type="submit" value="Search">
     </form> 
@@ -277,7 +278,6 @@ else:
     </html>
     """
     
-
 
 if __name__ == '__main__':
     enc_print("Content-Type:text/html;charset=utf-8;")
