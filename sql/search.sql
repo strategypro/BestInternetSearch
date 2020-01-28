71f8ea8ee70b43a52b0446577000bac17f0c86c4
Stan S <vfpro777@yahoo.com>
2020-01-28 11:05:37 -0500

    update sql

--- sql/search.sql
+++ sql/search.sql
@@ -1,15 +1,16 @@
 CREATE TABLE `search` (
 	`id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`site_address` TEXT NULL COLLATE 'utf8_general_ci',
-	`site_name` TEXT NULL COLLATE 'utf8_general_ci',
-	`site_title` TEXT NULL COLLATE 'utf8_general_ci',
-	`site_main_category` TEXT NULL COLLATE 'utf8_general_ci',
-	`site_keywords` TEXT NULL COLLATE 'utf8_general_ci',
-	`site_description` TEXT NULL COLLATE 'utf8_general_ci',
+	`site_address` TEXT(65535) NOT NULL COLLATE 'utf8_general_ci',
+	`site_name` TEXT(65535) NOT NULL COLLATE 'utf8_general_ci',
+	`site_title` TEXT(65535) NOT NULL COLLATE 'utf8_general_ci',
+	`site_main_category` TEXT(65535) NOT NULL COLLATE 'utf8_general_ci',
+	`site_keywords` TEXT(65535) NOT NULL COLLATE 'utf8_general_ci',
+	`site_description` TEXT(65535) NOT NULL COLLATE 'utf8_general_ci',
+	`site_favicon_uri` TEXT(65535) NOT NULL COLLATE 'utf8_general_ci',
 	PRIMARY KEY (`id`) USING BTREE
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=8
+AUTO_INCREMENT=1
 ;
 
